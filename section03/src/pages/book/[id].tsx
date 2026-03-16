ce7487d41ece02318a8df9b172657cbb57755c68
YunaJangqe <lis3277@gamil.com>
2026-03-16 20:45:49 +0900

    commit

--- section03/src/pages/book/[id].tsx
+++ section03/src/pages/book/[id].tsx
@@ -1,6 +1,8 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = () => {
     return {
@@ -9,13 +11,21 @@ export const getStaticPaths = () => {
             { params: { id: "2"} },
             { params: { id: "3"} },
         ],
-        fallback: false
+        //fallback: false : 404 Not found
+        //fallback: "blocking" : SSR 방식
+        fallback: true // SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
     }
 }
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
     const id = context.params!.id;
     const book = await fetchOneBook(Number(id));
+
+    if(!book) {
+        return {
+            notFound: true
+        }
+    }
 
     return {
         props: {
@@ -25,6 +35,19 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 };
 
 export default function Page({book}: InferGetStaticPropsType<typeof getStaticProps>) {
+    const router = useRouter();
+
+    if(router.isFallback) {
+        return <>
+            <Head>
+                <title>한입북스</title>
+                <meta property="og:image" content="/thumbnail.png" />
+                <meta property="og:title" content="한입북스" />
+                <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요" />
+            </Head>
+            <div>로딩중입니다</div>
+        </>
+    }
     if(!book) return "문제가 발생했습니다 다시 시도하세요";
 
     const {
@@ -37,14 +60,24 @@ export default function Page({book}: InferGetStaticPropsType<typeof getStaticPro
         coverImgUrl
     } = book;
 
-    return <div className={style.container}>
-        <div className={style.cover_img_container} 
-            style={{backgroundImage: `url('${coverImgUrl}')`}}>
-            <img src={coverImgUrl} />
-        </div>
-        <div className={style.title}>{title}</div>
-        <div className={style.subTitle}>{subTitle}</div>
-        <div className={style.author}>{author} | {publisher}</div>
-        <div className={style.description}>{description}</div>
-    </div>
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+                <meta property="og:image" content={coverImgUrl} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+            </Head>
+            <div className={style.container}>
+                <div className={style.cover_img_container} 
+                    style={{backgroundImage: `url('${coverImgUrl}')`}}>
+                    <img src={coverImgUrl} />
+                </div>
+                <div className={style.title}>{title}</div>
+                <div className={style.subTitle}>{subTitle}</div>
+                <div className={style.author}>{author} | {publisher}</div>
+                <div className={style.description}>{description}</div>
+            </div>
+        </>
+    )
 }
--- section03/src/pages/search/index.tsx
+++ section03/src/pages/search/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import BookItem from "@/components/book-item";
 import fetchBooks from "@/lib/fetch-books";
 import { BookData } from "@/types";
+import Head from "next/head";
 
 export default function Page() {
     const [books, setBooks] = useState<BookData[]>([]);
@@ -22,9 +23,17 @@ export default function Page() {
       }
     },[q])
 
-    return <div>
-      {books.map((book)=><BookItem key={book.id} {...book} />)}
-    </div>
+    return (
+      <div>
+        <Head>
+          <title>한입북스 - 검색결과</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스 - 검색결과" />
+          <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요" />
+        </Head>
+        {books.map((book)=><BookItem key={book.id} {...book} />)}
+      </div>
+    )
 
 }
 
