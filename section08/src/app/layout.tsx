1612621bacd6fd1f8f6a350eae259ebe4588242c
YunaJangqe <lis3277@gamil.com>
2026-03-23 21:06:45 +0900

    commit

---
+++ section08/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import "./globals.css";
+import Link from "next/link";
+import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_LOCAL_URL}/book`, {cache: "force-cache"});
+  if(!response.ok) {
+    return <div>오류가 발생했습니다...</div>
+  }
+  const books : BookData[] = await response.json();
+  const bookCount = books.length;
+  return <footer>
+            <div>제작 @winterlood</div>
+            <div>{bookCount}개의 도서가 등록되어있습니다</div>
+          </footer>
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className={style.container}>
+          <header>
+            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+          </header>
+          <main>{children}</main>          
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
